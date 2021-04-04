03887bd276146b7ccb46f6e4f2b15a8e97dd9338
Kumaraguru <19eucs071@skcet.ac.in>
2021-04-04 12:36:50 +0530

    adding QR code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/rendering.dart';
 import 'package:lottie/lottie.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:fancy_cursor/fancy_cursor.dart';
+import 'package:giffy_dialog/giffy_dialog.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,9 +27,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            body: WindowBorder(
-                color: borderColor, width: 1, child: Row(children: [Home()]))));
+        home: FancyCursor(
+          color: Colors.black,
+          curve: Curves.linear,
+          size: 20,
+          delay: Duration(milliseconds: 50),
+          child: Scaffold(
+              body: WindowBorder(
+                  color: borderColor,
+                  width: 1,
+                  child: Row(children: [Home()]))),
+        ));
   }
 }
 
@@ -41,43 +51,55 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  bool volume;
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    bool volume = true;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Expanded(
         child: MouseRegion(
-          //cursor: SystemMouseCursors.none,
-              child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [backgroundStartColor, backgroundEndColor],
-                    stops: [0.0, 1.0]),
-              ),
-              child: Column(children: [
-                WindowTitleBarBox(
-                    child: Row(children: [
-                  Expanded(child: MoveWindow()),
-                  //WindowButtons()
-                ])),
+      //cursor: SystemMouseCursors.none,
+      child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [backgroundStartColor, backgroundEndColor],
+                stops: [0.0, 1.0]),
+          ),
+          child: Column(children: [
+            WindowTitleBarBox(
+                child: Row(children: [
+              Expanded(child: MoveWindow()),
+              //WindowButtons()
+            ])),
 
-                // The home body starts here
-                Padding(
-                  padding: const EdgeInsets.only(left: 12, right: 12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
+            // The home body starts here
+            Padding(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
                     IconButton(
                       icon: Icon(
-                        Icons.add_circle,
+                        Icons.volume_mute_rounded,
                         color: Color(0xFF9A17CF),
                         size: 35,
                       ),
-                      onPressed: null,
+                      onPressed: () {
+                        setState(() {
+                          volume = !volume;
+                        });
+                      },
                     ),
                     Container(
-                      height: 120,
-                      width: 120,
-                      child: Lottie.asset('assets/voilet_eye.json')),
+                        height: 120,
+                        width: 120,
+                        child: Lottie.asset('assets/voilet_eye.json')),
                     Row(
                       children: [
                         IconButton(
@@ -86,99 +108,175 @@ class _HomeState extends State<Home> {
                             color: Color(0xFF9A17CF),
                             size: 35,
                           ),
-                          onPressed: null,
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => new AlertDialog(
+                                title: new Text(
+                                  'Connect your mobile device',
+                                  style: GoogleFonts.montserrat(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 25),
+                                ),
+                                content: Container(
+                                  height: 350,
+                                  child: Column(children: [
+                                    SizedBox(height: 20),
+                                    Lottie.asset(
+                                      'assets/qr.json',
+                                    ),
+                                    SizedBox(height: 50),
+                                    Text(
+                                  'Scan the below QR code with your mobile application client',
+                                  style: GoogleFonts.montserrat(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 15),
+                                ),
+                                SizedBox(height: 30),
+                                PrettyQr(data: "A3z4kMI",
+                                size: 120,
+                                roundEdges: true,)
+                                  ]),
+                                ),
+                                actions: <Widget>[
+                                  new FlatButton(
+                                    onPressed: () {
+                                      Navigator.of(context, rootNavigator: true)
+                                          .pop(); // dismisses only the dialog and returns nothing
+                                    },
+                                    child: new Text('OK'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
                         ),
                         SizedBox(width: 10)
                       ],
                     ),
                   ]),
+            ),
+            SizedBox(height: 20),
+            Text(
+                "Use your eyes to navigate through the options and blink 3 times to select",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 25)),
+            SizedBox(height: 50),
+            Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                Container(
+                  width: 500,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF9A17CF), Colors.purple])),
+                  child: Center(
+                      child: Text("Request for rest room",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25))),
                 ),
-                SizedBox(height: 20),
-                Text("Use your eyes to navigate through the options and blink 3 times to select",textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.black,fontWeight: FontWeight.w500, fontSize: 25)),
-                SizedBox(height: 50),
-                Column(
-                  children: [
-                    
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          width: 500,
-                          height: 120,
-                          decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          gradient: LinearGradient(colors: [Color(0xFF9A17CF), Colors.purple])
-                        ),
-                        child: Center(child: Text("Request for rest room",textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 25))),
-                        ),
-                        Container(
-                          width: 500,
-                          height: 120,
-                          decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          gradient: LinearGradient(colors: [Color(0xFF9A17CF), Colors.purple])
-                        ),
-                        child: Center(child: Text("Request for rest room",textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 25))),
-                        ),
-                        
-                      ]
-                    ),
-                    SizedBox(height: 70),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          width: 500,
-                          height: 120,
-                          decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          gradient: LinearGradient(colors: [Color(0xFF9A17CF), Colors.purple])
-                        ),
-                        child: Center(child: Text("Request for rest room",textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 25))),
-                        ),
-                        Container(
-                          width: 500,
-                          height: 120,
-                          decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          gradient: LinearGradient(colors: [Color(0xFF9A17CF), Colors.purple])
-                        ),
-                        child: Center(child: Text("Request for rest room",textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 25))),
-                        ),
-                        
-                      ]
-                    ),
-                                        SizedBox(height: 70),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          width: 500,
-                          height: 120,
-                          decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          gradient: LinearGradient(colors: [Color(0xFF9A17CF), Colors.purple])
-                        ),
-                        child: Center(child: Text("Request for rest room",textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 25))),
-                        ),
-                        Container(
-                          width: 500,
-                          height: 120,
-                          decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          gradient: LinearGradient(colors: [Color(0xFF9A17CF), Colors.purple])
-                        ),
-                        child: Center(child: Text("Custom request",textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 25))),
-                        ),
-                        
-                      ]
-                    ),
-                    SizedBox(height: 25),
-                    Text("The screen turns off automatically after 3 minutes of inactivity",textAlign: TextAlign.center, style: GoogleFonts.montserrat(color: Colors.black.withOpacity(0.6),fontWeight: FontWeight.w500, fontSize: 15)),
-                  ]
-                )
-              ])),
-        ));
+                Container(
+                  width: 500,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF9A17CF), Colors.purple])),
+                  child: Center(
+                      child: Text("Request for rest room",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25))),
+                ),
+              ]),
+              SizedBox(height: 70),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                Container(
+                  width: 500,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF9A17CF), Colors.purple])),
+                  child: Center(
+                      child: Text("Request for rest room",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25))),
+                ),
+                Container(
+                  width: 500,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF9A17CF), Colors.purple])),
+                  child: Center(
+                      child: Text("Request for rest room",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25))),
+                ),
+              ]),
+              SizedBox(height: 70),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                Container(
+                  width: 500,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF9A17CF), Colors.purple])),
+                  child: Center(
+                      child: Text("Request for rest room",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25))),
+                ),
+                Container(
+                  width: 500,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF9A17CF), Colors.purple])),
+                  child: Center(
+                      child: Text("Custom request",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25))),
+                ),
+              ]),
+              SizedBox(height: 25),
+              Text(
+                  "The screen turns off automatically after 3 minutes of inactivity",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.montserrat(
+                      color: Colors.black.withOpacity(0.6),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15)),
+            ])
+          ])),
+    ));
   }
 }
 
